5dae5ec474bbf12e62b7404910c8daf0446b1c74
Samuel Mittermair <samuel.mittermair@htl-wels.at>
2023-12-13 13:52:02 +0100

    removed comments

--- app/src/main/java/com/handson/handson/ui/quiz/Quiz.kt
+++ app/src/main/java/com/handson/handson/ui/quiz/Quiz.kt
@@ -108,12 +108,9 @@ fun Quiz(navController: NavController, quizViewModel: QuizViewModel = viewModel(
             BoxWithConstraints() {
                 Log.d("width", maxWidth.toString())
 
-
                 val configuration = LocalConfiguration.current
 
-// If our configuration changes then this will launch a new coroutine scope for it
                 LaunchedEffect(configuration) {
-                    // Save any changes to the orientation value on the configuration object
                     snapshotFlow { configuration.orientation }
                         .collect { orientation = it }
                 }
